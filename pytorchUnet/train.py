0d0847e8447c1b4eb29adc66bd61c2d8950b4977
bkahn-github <bk@tinymanager.com>
2018-02-25 13:47:49 -0600

    Add parameters to train

--- pytorchUnet/train.py
+++ pytorchUnet/train.py
@@ -8,11 +8,11 @@ from load_data import TrainDataset, TestDataset, load_data
 from model import UNet
 from metrics import dice_loss, iou
 
-def train(epochs=10):
-    model = UNet(3,1).cuda()
+def train(model, data_loader, loss, metric  epochs=10):
+    model = model
     optimizer = torch.optim.Adam(model.parameters(), lr = 1e-4)
 
-    _, _, _, _, _, _, train_dataloader, val_dataloader, _ = load_data()
+    _, _, _, _, _, _, train_dataloader, val_dataloader, _ = data_loader()
 
     for epoch in range(epochs):
         train_ious = []
@@ -26,8 +26,8 @@ def train(epochs=10):
             
             outputs = model(x_train)
 
-            loss = dice_loss(outputs, y_train)
-            train_iou, _ = iou((outputs.cpu().data.squeeze(0).numpy() > 0.5), y_train.cpu().data.numpy())
+            loss = loss(outputs, y_train)
+            train_iou, _ = metric((outputs.cpu().data.squeeze(0).numpy() > 0.5), y_train.cpu().data.numpy())
             train_ious.append(train_iou)
             
             loss.backward()
@@ -41,9 +41,11 @@ def train(epochs=10):
             
             o = model(x_val)
 
-            val_loss = dice_loss(o, y_val)
-            val_iou, _ = iou((o.cpu().data.squeeze(0).numpy() > 0.5), y_val.cpu().data.numpy())
+            val_loss = loss(o, y_val)
+            val_iou, _ = metric((o.cpu().data.squeeze(0).numpy() > 0.5), y_val.cpu().data.numpy())
             val_ious.append(val_iou)
 
     print('\n')
     print(f'Epoch: {epoch} Training Loss: {round(loss.data[0], 4)} Training IOU: {np.asarray(train_ious).mean()} Val Loss: {round(val_loss.data[0], 4)} Val IOU: {np.asarray(val_ious).mean()}', end='\n')
+
+    return model
