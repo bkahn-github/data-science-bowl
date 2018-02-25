fd31d051320180575e7dff523b4141db35c9be45
bkahn-github <bk@tinymanager.com>
2018-02-25 13:18:44 -0600

    Add submit.py

---
+++ pytorchUnet/submit.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def submit(new_test_ids, rles):
+    sub = pd.DataFrame()
+    sub['ImageId'] = new_test_ids
+    sub['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
+    sub.to_csv('submission.csv', index=False)
