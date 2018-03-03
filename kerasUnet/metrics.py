0feeb02a566f51d3c9cc41c98aae2baf18349bd8
bkahn-github <bk@tinymanager.com>
2018-03-02 20:40:54 -0600

    Add and use keras_iou function

--- kerasUnet/metrics.py
+++ kerasUnet/metrics.py
@@ -9,6 +9,17 @@ def dice_loss(inputs, targets):
     score = 1 - score.sum()/num
     return score
 
+def keras_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
 def iou(predict, label):
 
     # Precision helper function
--- kerasUnet/old/model.py
+++ kerasUnet/old/model.py
@@ -1,7 +1,7 @@
 from keras.models import Model
 from keras.layers import Input, Lambda, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate, Dropout
 
-from metrics import my_iou_metric
+from metrics import keras_iou
 
 def model():
     input_conv = Input((128, 128, 3))
@@ -53,7 +53,7 @@ def model():
     outputs = Conv2D(1, (1, 1), activation='sigmoid') (conv_21)
 
     model = Model(inputs=input_conv, outputs=outputs)
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[my_iou_metric])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[keras_iou])
     model.summary()
 
     return model
