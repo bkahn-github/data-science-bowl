c3ce6350dc17550d44bd4a3ffafb1f02aed933fc
bkahn-github <bk@tinymanager.com>
2018-03-06 17:15:11 -0600

    Fix process_data code

--- pytorchUnet/process_data.py
+++ pytorchUnet/process_data.py
@@ -4,8 +4,10 @@ from tqdm import tqdm
 import torch
 from torch.autograd import Variable
 
+import skimage
+import skimage.io
+import skimage.morphology
 from skimage.transform import resize
-from skimage.morphology import label
 
 def predict(model, test_dataset, test_dataloader, optimizer):
     preds = np.zeros((len(test_dataset), 128, 128))
@@ -19,11 +21,6 @@ def predict(model, test_dataset, test_dataloader, optimizer):
 
     return preds
 
-def threshold_preds(preds, threshold=0.5):
-    preds = (preds > threshold).astype(np.uint8)
-    
-    return preds
-
 def rle_encoding(x):
     dots = np.where(x.T.flatten() == 1)[0]
     run_lengths = []
@@ -34,23 +31,25 @@ def rle_encoding(x):
         prev = b
     return run_lengths
 
-def upsample(preds, x_test_sizes):
-    preds_test_upsampled = []
-    for i in range(len(preds)):
-        preds_test_upsampled.append(resize(np.squeeze(preds[i]), x_test_sizes[i], mode='constant', preserve_range=True).astype(np.uint8))
-
-    return preds_test_upsampled
-
 def prob_to_rles(x, cutoff=0.5):
-    lab_img = label(x > cutoff)
+    lab_img = skimage.morphology.label(x > cutoff)
     for i in range(1, lab_img.max() + 1):
         yield rle_encoding(lab_img == i)
+
+def upsample(preds, test_path=test_path, test_ids=test_ids):
+    preds_test_upsampled = []
+    for i, test_id in enumerate(test_ids):
+        img = skimage.io.imread('{0}/{1}/images/{1}.png'.format(test_path, test_id))
+        img_upscaled = skimage.transform.resize(preds[i], (img.shape[0], img.shape[1]), mode='constant', preserve_range=True)
+        preds_test_upsampled.append(img_upscaled)
+
+    return preds_test_upsampled
 
 def encode(preds_test_upsampled, test_ids):
     new_test_ids = []
     rles = []
     for n, id_ in enumerate(test_ids):
-        rle = list(prob_to_rles(preds_test_upsampled[n]))
+    rle = list(prob_to_rles(preds_test_upsampled[n]))
         rles.extend(rle)
         new_test_ids.extend([id_] * len(rle))
 
