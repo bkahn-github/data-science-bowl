4825785d956b0f206d64ebb56e823f513040d9d9
bkahn-github <bk@tinymanager.com>
2018-03-08 19:25:54 -0600

    Reimplement a Unet in pytorch
    * Correct formatting and code
    
    * Fix process_data code
    
    * Fix incorrect indent in process_data
    
    * Add missing variables and imports to process_data
    
    * Convert images from torch tensor to uint8
    
    * Convert numpy array to torch tensors
    
    * Edit datasets
    
    * Use original load_data
    
    * Add submission and notebook of correct implementation
    
    * Remove folders with incorrect implementations
    
    * Remove old notebook
    
    * Add notebook and submission for 10 epochs
    
    * Remove extra long outputs from notebook

--- pytorchUnet/process_data.py
+++ pytorchUnet/process_data.py
@@ -1,11 +1,25 @@
+import os
+
 import numpy as np
 from tqdm import tqdm
 
 import torch
 from torch.autograd import Variable
 
+import skimage
+import skimage.io
+import skimage.morphology
 from skimage.transform import resize
-from skimage.morphology import label
+
+if os.environ.get('platform') == 'surface':
+    train_path = '/home/bilal/.kaggle/competitions/data-science-bowl-2018/train/'
+    test_path = '/home/bilal/.kaggle/competitions/data-science-bowl-2018/test/'
+else:    
+    train_path = '../../.kaggle/competitions/data-science-bowl-2018/train/'
+    test_path = '../../.kaggle/competitions/data-science-bowl-2018/test/'
+
+train_ids = next(os.walk(train_path))[1]
+test_ids = next(os.walk(test_path))[1]
 
 def predict(model, test_dataset, test_dataloader, optimizer):
     preds = np.zeros((len(test_dataset), 128, 128))
@@ -19,11 +33,6 @@ def predict(model, test_dataset, test_dataloader, optimizer):
 
     return preds
 
-def threshold_preds(preds, threshold=0.5):
-    preds = (preds > threshold).astype(np.uint8)
-    
-    return preds
-
 def rle_encoding(x):
     dots = np.where(x.T.flatten() == 1)[0]
     run_lengths = []
@@ -34,17 +43,19 @@ def rle_encoding(x):
         prev = b
     return run_lengths
 
-def upsample(preds, x_test_sizes):
-    preds_test_upsampled = []
-    for i in range(len(preds)):
-        preds_test_upsampled.append(resize(np.squeeze(preds[i]), x_test_sizes[i], mode='constant', preserve_range=True).astype(np.uint8))
-
-    return preds_test_upsampled
-
 def prob_to_rles(x, cutoff=0.5):
-    lab_img = label(x > cutoff)
+    lab_img = skimage.morphology.label(x > cutoff)
     for i in range(1, lab_img.max() + 1):
         yield rle_encoding(lab_img == i)
+
+def upsample(preds, test_path=test_path, test_ids=test_ids):
+    preds_test_upsampled = []
+    for i, test_id in enumerate(test_ids):
+        img = skimage.io.imread('{0}/{1}/images/{1}.png'.format(test_path, test_id))
+        img_upscaled = skimage.transform.resize(preds[i], (img.shape[0], img.shape[1]), mode='constant', preserve_range=True)
+        preds_test_upsampled.append(img_upscaled)
+
+    return preds_test_upsampled
 
 def encode(preds_test_upsampled, test_ids):
     new_test_ids = []
