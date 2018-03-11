202d7fbaaf301e7fef45299648ea70be5c3efdfb
bkahn-github <bk@tinymanager.com>
2018-03-11 17:09:41 -0500

    Implement a Keras unet with an image size of 256 by 256
    * Modify files for a unet with an input size of 256 by 256
    
    * Remove metric from unet
    
    * Remove keras-iou metric from metrics
    
    * Add submission and notebook for 25 epochs 256x256 batch size 4
    
    * Add submission for batch size of 8
    
    * Remove unused visualize.py file
    
    * Add submission for 50 epochs

---
+++ KerasUnet256x256/metrics.py
@@ -0,0 +1,75 @@
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+
+import skimage
+
+def dice_loss(inputs, targets):
+    num = targets.size(0)
+    m1  = inputs.view(num,-1)
+    m2  = targets.view(num,-1)
+    intersection = (m1 * m2)
+    score = 2. * (intersection.sum(1)+1) / (m1.sum(1) + m2.sum(1)+1)
+    score = 1 - score.sum()/num
+    return score
+
+def iou(predict, label):
+
+    # Precision helper function
+    def compute_precision(threshold, iou):
+        matches = iou > threshold
+        true_positives  = np.sum(matches, axis=1) == 1  # Correct objects
+        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        return tp, fp, fn
+
+    num_label   = len(np.unique(label  ))
+    num_predict = len(np.unique(predict))
+
+    # Compute intersection between all objects
+    intersection = np.histogram2d(label.flatten(), predict.flatten(), bins=(num_label, num_predict))[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(label,   bins = num_label  )[0]
+    area_pred = np.histogram(predict, bins = num_predict)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred,  0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+
+    # Exclude background from the analysis
+    intersection = intersection[1:,1:]
+    union = union[1:,1:]
+    union[union == 0] = 1e-9
+
+    # Compute the intersection over union
+    iou = intersection / union
+
+    precision = []
+    average_precision = 0
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = compute_precision(t, iou)
+        p = tp / (tp + fp + fn)
+        precision.append((t, p, tp, fp, fn))
+        average_precision += p
+
+    average_precision /= len(precision)
+    return average_precision, precision
+
+def iou_metric(x_train, y_train, x_val, y_val, unet):
+    train_preds = unet.predict(x_train)
+    val_preds = unet.predict(x_val)
+    
+    train_ious = []
+    for i in range(0, train_preds.shape[0]):
+        temp = iou(skimage.morphology.label(train_preds[i] > 0.5), skimage.morphology.label(y_train[i]))[0]
+        train_ious.append(temp)
+
+    val_ious = []
+    for i in range(0, val_preds.shape[0]):
+        temp = iou(skimage.morphology.label(val_preds[i] > 0.5), skimage.morphology.label(y_val[i]))[0]
+        val_ious.append(temp)
+
+    return np.asarray(train_ious).mean(), np.asarray(val_ious).mean()
