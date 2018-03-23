facfb7a21f955fac42e6129f64eaf299ab2623c3
bkahn-github <bk@tinymanager.com>
2018-03-23 12:43:10 -0500

    Use alternate config

--- maskRcnn/bowl_config.py
+++ maskRcnn/bowl_config.py
@@ -1,45 +1,64 @@
 from config import Config
 
-class BowlConfig(Config):
-    """Configuration for training on the toy shapes dataset.
-    Derives from the base Config class and overrides values specific
-    to the toy shapes dataset.
-    """
-    # Give the configuration a recognizable name
-    NAME = "bowl"
+class BowlConfig(Config): 
+    NAME = "bowl" 
+    GPU_COUNT = 1 
+    IMAGES_PER_GPU = 1 
+    NUM_CLASSES = 1 + 1 
+    IMAGE_MIN_DIM = 256 
+    IMAGE_MAX_DIM = 512 
+    RPN_ANCHOR_SCALES = (4, 8, 16, 32, 64) 
+    TRAIN_ROIS_PER_IMAGE = 500 
+    STEPS_PER_EPOCH = 600 // (IMAGES_PER_GPU * GPU_COUNT) 
+    VALIDATION_STEPS = 70 // (IMAGES_PER_GPU * GPU_COUNT)
+    MEAN_PIXEL = [0, 0, 0] 
+    LEARNING_RATE = 0.01 
+    USE_MINI_MASK = True 
+    MAX_GT_INSTANCES = 500
 
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 2
 
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 1 # background + nuclei
+# from config import Config
 
-    # Use small images for faster training. Set the limits of the small side
-    # the large side, and that determines the image shape.
-    IMAGE_MIN_DIM = 512
-    IMAGE_MAX_DIM = 512
+# class BowlConfig(Config):
+#     """Configuration for training on the toy shapes dataset.
+#     Derives from the base Config class and overrides values specific
+#     to the toy shapes dataset.
+#     """
+#     # Give the configuration a recognizable name
+#     NAME = "bowl"
 
-    # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+#     # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+#     # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+#     GPU_COUNT = 1
+#     IMAGES_PER_GPU = 2
 
-    # Reduce training ROIs per image because the images are small and have
-    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 600
+#     # Number of classes (including background)
+#     NUM_CLASSES = 1 + 1 # background + nuclei
 
-    STEPS_PER_EPOCH = None
+#     # Use small images for faster training. Set the limits of the small side
+#     # the large side, and that determines the image shape.
+#     IMAGE_MIN_DIM = 512
+#     IMAGE_MAX_DIM = 512
 
-    # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 5
+#     # Use smaller anchors because our image and objects are small
+#     RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
 
-    USE_MINI_MASK = True
+#     # Reduce training ROIs per image because the images are small and have
+#     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+#     TRAIN_ROIS_PER_IMAGE = 600
 
-    MAX_GT_INSTANCES = 256
+#     STEPS_PER_EPOCH = None
 
-    DETECTION_MAX_INSTANCES = 512
+#     # use small validation steps since the epoch is small
+#     VALIDATION_STEPS = 5
 
-    RESNET_ARCHITECTURE = "resnet50"
+#     USE_MINI_MASK = True
+
+#     MAX_GT_INSTANCES = 256
+
+#     DETECTION_MAX_INSTANCES = 512
+
+#     RESNET_ARCHITECTURE = "resnet50"
 
 
 bowl_config = BowlConfig()
