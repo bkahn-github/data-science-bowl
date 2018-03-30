ea45ce1b72e1c9acdc8240c231d37a06645377ac
bkahn-github <bk@tinymanager.com>
2018-03-30 11:19:24 -0500

    Add create_centers function

--- pytorchMultiOutputUnet/create_masks.py
+++ pytorchMultiOutputUnet/create_masks.py
@@ -6,6 +6,7 @@ from imageio import imwrite
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from scipy import ndimage
 
 def create_masks(root_folder, stage_number, stage_section, output_folder, subset=False):
     stage_folder = os.path.join(root_folder, 'stage' + stage_number + '_' + stage_section) 
@@ -56,4 +57,29 @@ def create_contours(root_folder, stage_number, stage_section, output_folder, sub
         contour_path = os.path.join(stage_folder + '_contours', mask_id + '.png')        
         imwrite(contour_path, total_mask)
 
-# def create_centers(root_folder, stage_folder, output_folder):
+def create_centers(root_folder, stage_number, stage_section, output_folder, subset=False):
+    stage_folder = os.path.join(root_folder, 'stage' + stage_number + '_' + stage_section) 
+    os.makedirs(stage_folder + '_centers', exist_ok=True)
+
+    if subset:
+        masks_folder = glob.glob(os.path.join(stage_folder, '*'))[:10]
+    else:
+        masks_folder = glob.glob(os.path.join(stage_folder, '*'))        
+    
+    for mask_folder in tqdm(masks_folder):
+        mask_id = mask_folder.split('/')[-1]
+        masks = []
+        for mask in glob.glob(os.path.join(mask_folder, 'masks/*')):
+            img = Image.open(mask)
+            img = np.asarray(img)
+            img = img / 255.0
+
+            img_center = np.zeros_like(img).astype(np.uint8)
+            x, y = ndimage.measurements.center_of_mass(img)
+            cv2.Circle(img_center, (x, y), 4, (255, 255, 255), -1)
+
+            masks.append(img_center)
+        total_mask = np.where(np.sum(masks, axis=0) > 128., 255., 0.).astype(np.uint8)        
+
+        center_path = os.path.join(stage_folder + '_centers', mask_id + '.png')        
+        imwrite(center_path, total_mask)
