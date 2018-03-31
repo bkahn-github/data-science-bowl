7fc905408e03253d69676017b3cf0b5be2545952
bkahn-github <bk@tinymanager.com>
2018-03-31 11:57:03 -0500

    Convert RGBA images to RGB

--- pytorchMultiOutputUnet/loaders.py
+++ pytorchMultiOutputUnet/loaders.py
@@ -9,6 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
 from skimage import io, transform
+from PIL import Image
 
 class TrainDataset(Dataset):
     def __init__(self, stage, root_folder, imgs_folder, mask_folder, contours_folder, centers_folder, subset=False, transform=None):
@@ -46,7 +47,14 @@ class TrainDataset(Dataset):
         contour_path = os.path.join(self.root_folder, 'stage' + self.stage + '_train_contours', id + '.png')
         center_path = os.path.join(self.root_folder, 'stage' + self.stage + '_train_centers', id + '.png')
         
-        img = io.imread(img_path)
+        img = Image.open(img_path)
+        img.load()
+
+        RGBimg = Image.new("RGB", img.size, (255, 255, 255))
+        RGBimg.paste(img, mask=img.split()[3])
+        
+        img = np.array(RGBimg)
+        
         mask = io.imread(mask_path)
         contour = io.imread(contour_path)
         center = io.imread(center_path)
