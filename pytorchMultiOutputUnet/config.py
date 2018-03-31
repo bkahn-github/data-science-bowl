5e3f1d50a0f97e92d42ec6499b0b146c3dcd6267
bkahn-github <bk@tinymanager.com>
2018-03-31 13:00:23 -0500

    Add config file to store global variables

---
+++ pytorchMultiOutputUnet/config.py
@@ -0,0 +1,11 @@
+root_folder = '/home/bilal/.kaggle/competitions/data-science-bowl-2018/'
+
+stage='1'
+
+imgs_folder = 'stage1_train'
+
+masks_output_folder = 'stage1_masks'
+contours_output_folder = 'stage1_contours'
+centers_output_folder = 'stage1_centers'
+
+subset = True
