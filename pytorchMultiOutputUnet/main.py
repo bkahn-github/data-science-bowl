bd72b8d5f157dab05bf4599a8386121b723e8a1e
bkahn-github <bk@tinymanager.com>
2018-03-30 20:35:57 -0500

    temp2

--- pytorchMultiOutputUnet/main.py
+++ pytorchMultiOutputUnet/main.py
@@ -5,7 +5,8 @@ import logging
 from create_masks import create_masks
 from loaders import TrainDataset, train_transforms
 
-root_folder = '/home/bilal/.kaggle/competitions/data-science-bowl-2018/'
+# root_folder = '/home/bilal/.kaggle/competitions/data-science-bowl-2018/'
+root_folder = '~/.kaggle/competitions/data-science-bowl-2018/'
 
 imgs_folder = 'stage1_train'
 
