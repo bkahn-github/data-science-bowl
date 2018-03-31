b6e7077300d51789170a3f2af0348b65176b83d3
bkahn-github <bk@tinymanager.com>
2018-03-31 17:53:08 -0500

    Add training code to main.py

--- pytorchMultiOutputUnet/main.py
+++ pytorchMultiOutputUnet/main.py
@@ -10,7 +10,12 @@ from torch.utils.data import DataLoader
 from config import config
 from create_masks import create_masks
 from loaders import TrainDataset, train_transforms
+from model import Unet
 from utils import get_ids
+
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
     
 @click.group()
 def action():
@@ -37,8 +42,26 @@ def train():
     train = TrainDataset(ids, transform=train_transforms)
     trainDataloader = DataLoader(train, batch_size=config.BATCH_SIZE, shuffle=config.SHUFFLE, num_workers=config.NUM_WORKERS)
 
-    print(train[0])
+    model = Unet()
 
+    criterion = nn.BCELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    for i, data in enumerate(trainDataloader):
+        img, mask, contour, center = data['img'], data['mask'], data['contour'], data['center']
+
+        x = Variable(img)
+        y = Variable(mask)
+
+        optimizer.zero_grad()
+
+        out = model(x)
+
+        loss = criterion(out, y)
+        print(loss.data[0])
+        loss.backward()
+        optimizer.step()
+        
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s >>> %(message)s',datefmt='%Y-%m-%d %H-%M-%S')
     logging.info('Started the program')
