571791afd03672d9784c01c5ba38636b8aeb576a
bkahn-github <bk@tinymanager.com>
2018-04-14 10:29:12 -0500

    Try moving each outconv into separate blocks

--- pytorchMultiOutputUnet/model.py
+++ pytorchMultiOutputUnet/model.py
@@ -82,7 +82,10 @@ class Unet(nn.Module):
         self.up_3 = Upsample(64, 32)
         self.up_4 = Upsample(32, 16)
 
-        self.out_convs = nn.ModuleList([OutConv(16, 1), OutConv(16, 1), OutConv(16, 1)])
+        # self.out_convs = nn.ModuleList([OutConv(16, 1), OutConv(16, 1), OutConv(16, 1)])
+        self.out_conv_1 = OutConv(16, 1)
+        self.out_conv_2 = OutConv(16, 1)
+        self.out_conv_3 = OutConv(16, 1)
 
     def forward(self, x):
         x1 = self.in_conv(x)
@@ -98,6 +101,7 @@ class Unet(nn.Module):
         x9 = self.up_3(x8, x2)
         x10 = self.up_4(x9, x1)
 
-        outputs = [output(x10) for output in self.out_convs]
+        # outputs = [output(x10) for output in self.out_convs]
+        outputs = [self.out_conv_1(x), self.out_conv_2(x), self.out_conv_3(x)]
 
         return outputs
