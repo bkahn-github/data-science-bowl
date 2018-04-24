2e5303cabfd41784c33fd6b4fdbb39364fa9181d
bkahn-github <bk@tinymanager.com>
2018-04-24 12:33:46 -0500

    Move save model functionality to function

--- pytorchMultiOutputUnet/main.py
+++ pytorchMultiOutputUnet/main.py
@@ -15,7 +15,7 @@ from loaders import TrainDataset, x_transforms, y_transforms
 from model import Unet
 from visualize import show_images
 from metrics import dice_loss
-from utils import get_ids, print_losses, EarlyStopping
+from utils import get_ids, print_losses, save_model, EarlyStopping
 
 import torch.nn as nn
 from torch.autograd import Variable
@@ -116,10 +116,8 @@ def train(epochs, weights):
             val_loss = dice_loss(outs, y)
 
         print_losses(train_loss, val_loss, epoch)
-
-        torch.save(model.state_dict(), './model-' + str(epoch) + '.pt')
-
         early_stopping.check(val_loss, epoch)
+        save_model(model)
 
 def visualize(weights, subset):
     if subset == 'True':
