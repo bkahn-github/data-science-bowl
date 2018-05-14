6b77d3b43da239752db67bf28ad77833011f9b47
bkahn-github <bk@tinymanager.com>
2018-05-14 17:12:23 -0500

    Use new augmentation

--- pytorchMultiOutputUnet/loaders.py
+++ pytorchMultiOutputUnet/loaders.py
@@ -1,13 +1,11 @@
 import os
 import glob
 import cv2
+import random
 import numpy as np
 
 import torch
 import torchvision
-
-import imgaug
-from imgaug import augmenters as iaa
 
 from utils import get_path
 
@@ -17,31 +15,60 @@ from torchvision import transforms, utils
 from skimage import io, transform
 from PIL import Image
 
-seq = iaa.Sequential([iaa.Noop()])
+class RandomCrop(object):
+    def __call__(self, sample, size=0.5):
+        img = sample[0]
+        mask = sample[1]
 
-def randomCrop(image, mask, output_size):
-    h, w = image.shape[:2]
-    new_h, new_w = output_size, output_size
-    
-    if h != new_h:
-        top = np.random.randint(0, h - new_h)
-        left = np.random.randint(0, w - new_w)
-    else:
-        top = 0
-        left = 0
+        h, w = img.shape[:2]
+
+        if h != size:
+            top = np.random.randint(0, h - size)
+            left = np.random.randint(0, w - size)
+        else:
+            top = 0
+            left = 0
+
+        img = img[top: top + size, left: left + size]
+        mask = mask[top: top + size, left: left + size]
+
+        return img, mask
+
+class FlipLR(object):
+    def __call__(self, sample, p):
+        img, mask = sample[0], sample[1]
         
-    image = image[top: top + new_h, left: left + new_w]
-    mask = mask[top: top + new_h, left: left + new_w]
+        if random.random() < p:
+            img = img[:, ::-1].copy()
+            mask = mask[:, ::-1].copy()
+            return img, mask
+        return img, mask
 
-    return image, mask
+class ToTensor(object):
+    def __call__(self, sample):
+        img, mask = sample[0], sample[1]
+        
+        img = transforms.ToTensor()(img)
+        mask = transforms.ToTensor()(mask)
+        
+        return img, mask
+    
+def augmentation(img, mask):
+    randomCrop = RandomCrop()
+    toTensor = ToTensor()
+    flipLR = FlipLR()
+    
+    img, mask = randomCrop([img, mask], 256)
+    img, mask = flipLR([img, mask], 0.5)
+    img, mask = toTensor([img, mask])    
+
+    return img, mask
 
 class TrainDataset(Dataset):
-    def __init__(self, ids, x_transform=None, target_transforms=None):
+    def __init__(self, ids, augmentation=None):
         
         self.ids = ids
-
-        self.x_transform = x_transform
-        self.target_transforms = target_transforms
+        self.augmentation = augmentation
 
     def __len__(self):
         return len(self.ids)
@@ -54,25 +81,6 @@ class TrainDataset(Dataset):
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         target = cv2.imread(targets_path, cv2.IMREAD_COLOR)
         
-        seq_det = seq.to_deterministic()
-        img = seq_det.augment_image(img)
-        target = seq_det.augment_image(target)
-
-        img, target = randomCrop(img, target, 256)
-
-        img = self.x_transform(img)
-        target = self.target_transforms(target)
+        img, target = self.augmentation(img, target)
 
         return {'img': img, 'target': target}
-
-
-x_transforms = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-])
-
-target_transforms = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.ToTensor()
-])
