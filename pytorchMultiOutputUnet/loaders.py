69044881cfaee556798605292b76e32d00493121
bkahn-github <bk@tinymanager.com>
2018-05-15 16:16:23 -0500

    Add flipUD

--- pytorchMultiOutputUnet/loaders.py
+++ pytorchMultiOutputUnet/loaders.py
@@ -44,6 +44,16 @@ class FlipLR(object):
             return img, mask
         return img, mask
 
+class FlipUD(object):
+    def __call__(self, sample, p):
+        img, mask = sample[0], sample[1]
+        
+        if random.random() < p:
+            img = img[::-1].copy()
+            mask = mask[::-1].copy()
+            return img, mask
+        return img, mask
+
 class ToTensor(object):
     def __call__(self, sample):
         img, mask = sample[0], sample[1]
@@ -57,9 +67,11 @@ def augmentation(img, mask):
     randomCrop = RandomCrop()
     toTensor = ToTensor()
     flipLR = FlipLR()
+    flipUD = FlipUD()
     
     img, mask = randomCrop([img, mask], 256)
     img, mask = flipLR([img, mask], 0.5)
+    img, mask = flipUD([img, mask], 0.5)
     img, mask = toTensor([img, mask])    
 
     return img, mask
